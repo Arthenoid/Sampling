e8368d031c8d70dcf3a20f7f1a00f191877f38e3
Arthenoid <61414266+Arthenoid@users.noreply.github.com>
2023-12-21 01:16:31 +0100

    Repeated distinct sampling

--- src/arthenoid/hellwire/sampling/DistinctSampler.java
+++ src/arthenoid/hellwire/sampling/DistinctSampler.java
@@ -8,30 +8,51 @@ public class DistinctSampler {
   protected final Context context;
   protected final long n;
   protected final int log2n;
-  protected final Hash[] h;
-  protected final SparseRecoverer[] D;
+  protected final Subsampler[] subsamplers;
+  
+  protected class Subsampler {
+    protected final Hash[] h;
+    protected final SparseRecoverer[] D;
+    
+    public Subsampler() {
+      h = new Hash[log2n + 1];
+      D = new SparseRecoverer[log2n + 1];
+      for (int ℓ = 0; ℓ <= log2n; ℓ++) {
+        h[ℓ] = context.newHash();
+        D[ℓ] = new SparseRecoverer(context, n);
+      }
+    }
+    
+    public void update(long i, long w) {
+      for (int ℓ = 0; ℓ <= log2n; ℓ++) if (h[ℓ].toRange(i, 1 << ℓ) == 0) D[ℓ].update(i, w);
+    }
+    
+    public IntegerResult query() {
+      for (int ℓ = 0; ℓ <= log2n; ℓ++) {
+        IntegerResult res = D[ℓ].query();
+        if (res != null && res.weight != 0) return res;
+      }
+      return null;
+    }
+  }
   
   public DistinctSampler(Context context, long n) {
     this.context = context;
     this.n = n;
     log2n = Long.SIZE - Long.numberOfLeadingZeros(n - 1);
-    h = new Hash[log2n + 1];
-    D = new SparseRecoverer[log2n + 1];
-    for (int ℓ = 0; ℓ <= log2n; ℓ++) {
-      h[ℓ] = context.newHash();
-      D[ℓ] = new SparseRecoverer(context, n);
-    }
+    subsamplers = new Subsampler[log2n]; //TODO Choose DistinctSampler repetitions number
+    for (int i = 0; i < subsamplers.length; i++) subsamplers[i] = new Subsampler();
   }
   
   public void update(long i, long w) {
     if (i < 0 || i >= n) throw new IllegalArgumentException();
-    for (int ℓ = 0; ℓ <= log2n; ℓ++) if (h[ℓ].toRange(i, 1 << ℓ) == 0) D[ℓ].update(i, w);
+    for (Subsampler subsampler : subsamplers) subsampler.update(i, w);
   }
   
   public IntegerResult query() {
-    for (int ℓ = 0; ℓ <= log2n; ℓ++) {
-      IntegerResult res = D[ℓ].query();
-      if (res != null && res.weight != 0) return res;
+    for (Subsampler subsampler : subsamplers) {
+      IntegerResult res = subsampler.query();
+      if (res != null) return res;
     }
     return null;
   }
