89b0143a07296b6cd847491860339386c4e09005
Arthenoid <61414266+Arthenoid@users.noreply.github.com>
2024-06-28 21:08:46 +0200

    Optimise PrecisionSampler

--- src/arthenoid/hellwire/sampling/samplers/PrecisionSampler.java
+++ src/arthenoid/hellwire/sampling/samplers/PrecisionSampler.java
@@ -23,28 +23,27 @@ public class PrecisionSampler implements Sampler {
   protected final int Dt;
   protected final int Dd;
   protected final Subsampler[] subsamplers;
+  protected final CountSketch R;
   
   @Override
   public int memoryUsed() {
-    int m = 8 + subsamplers.length;
-    for (Subsampler subsampler : subsamplers) m += subsampler.memoryUsed();
-    return m;
+    int mem = 9 + subsamplers.length + R.memoryUsed();
+    for (Subsampler subsampler : subsamplers) mem += subsampler.memoryUsed();
+    return mem;
   }
   
   protected class Subsampler implements MemoryUser {
     protected final Hash uH;
     protected final CountSketch D;
-    protected final CountSketch R;
     
     @Override
     public int memoryUsed() {
-      return 3 + uH.memoryUsed() + D.memoryUsed() + R.memoryUsed();
+      return 2 + uH.memoryUsed() + D.memoryUsed();
     }
     
     protected Subsampler() {
       uH = context.newHash();
       D = new CountSketch(context, Dt, Dd);
-      R = new CountSketch(context, Dt, Dd);
     }
     
     protected double u(long i) {
@@ -54,24 +53,21 @@ public class PrecisionSampler implements Sampler {
     
     public void update(long i, double w) {
       D.update(i, w / Math.sqrt(u(i)));
-      R.update(i, w);
     }
     
-    public Result query() {
+    public Result query(double r) {
       PriorityQueue<Result> heap = new PriorityQueue<>((r1, r2) -> Double.compare(Math.abs(r1.w), Math.abs(r2.w)));
       for (long i = 0; i < m; i++) heap.add(new Result(i, D.query(i)));
+      double s = 0;
       for (long i = m; i < n; i++) {
         heap.add(new Result(i, D.query(i)));
-        heap.remove();
+        double w = heap.remove().w;
+        s += w * w;
       }
+      s = Math.sqrt(s);
       Result[] top = heap.toArray(Result[]::new);
       Result peak = top[0];
-      for (int i = 0; i < m; i++) {
-        D.update(top[i].i, -top[i].w);
-        if (Math.abs(top[i].w) > Math.abs(peak.w)) peak = top[i];
-      }
-      double s = D.norm(2), r = R.norm(2);
-      for (int i = 0; i < m; i++) D.update(top[i].i, top[i].w);
+      for (int i = 1; i < m; i++) if (Math.abs(top[i].w) > Math.abs(peak.w)) peak = top[i];
       return s > Math.sqrt(ε * m) * r || Math.abs(peak.w) < r / Math.sqrt(ε) ? null : new Result(peak.i, peak.w * Math.sqrt(u(peak.i)));
     }
   }
@@ -88,19 +84,21 @@ public class PrecisionSampler implements Sampler {
     Dd = (int) Math.round(logN);
     subsamplers = new Subsampler[(int) Math.round(Math.log(1 / δ) / ε)];
     for (int i = 0; i < subsamplers.length; i++) subsamplers[i] = new Subsampler();
+    R = new CountSketch(context, Dt, Dd);
   }
   
   @Override
   public void update(long i, long w) {
-    if (i < 0 || i >= n) throw new IllegalArgumentException("Item outside of range (" + n + "): " + i);
     double ww = w;
+    R.update(i, ww);
     for (Subsampler subsampler : subsamplers) subsampler.update(i, ww);
   }
   
   @Override
   public Result query() {
+    double r = R.norm(2);
     for (Subsampler subsampler : subsamplers) {
-      Result res = subsampler.query();
+      Result res = subsampler.query(r);
       if (res != null) return res;
     }
     return null;
@@ -108,6 +106,7 @@ public class PrecisionSampler implements Sampler {
   
   @Override
   public Stream<Result> queryAll() {
-    return Stream.of(subsamplers).map(Subsampler::query);
+    double r = R.norm(2);
+    return Stream.of(subsamplers).map(s -> s.query(r));
   }
 }
